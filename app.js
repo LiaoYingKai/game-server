879c12fc01a50b1206793ab6963471c4a53596ce
kai <b10223052@yuntech.org.tw>
2019-11-01 13:47:29 +0800

    add join or create room

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const server = require('http').Server(app)
 	.listen(8888, () => {console.log('open Server')})
 
 const io = require('socket.io')(server)
+const roomList = {}
 
 io.on('connection', socket => {
 	console.log('connection')
@@ -13,4 +14,27 @@ io.on('connection', socket => {
 	socket.on('disconnect', (reason) => {
 		console.log(reason)
 	})
+	const room = joinOrCreate() || getRoomName()
+	if(joinOrCreate()) {
+		socket.join(room, () => {
+			roomList[room].push(socketID)
+			if(roomList[room].length === 2) {
+				io.to(room).emit('startGame')
+			}
+		})
+	} else {
+		socket.join(room, () => {
+			roomList[room] = [socketID]
+			console.log(roomList)
+		})
+	}
 })
+
+
+function getRoomName() {
+	return Math.floor(Math.random()*10000)
+}
+
+function joinOrCreate() {
+	return Object.keys(roomList).filter(key => roomList[key].length < 2)[0]
+}
