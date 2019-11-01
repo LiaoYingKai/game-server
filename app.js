bdd58df73bf84dda06a2b7b333fdd101f0817715
kai <b10223052@yuntech.org.tw>
2019-11-01 14:02:44 +0800

    add disconnect doing

--- app.js
+++ app.js
@@ -11,15 +11,14 @@ io.on('connection', socket => {
 	console.log('connection')
 	const socketID = socket.id;
 	console.log(socketID)
-	socket.on('disconnect', (reason) => {
-		console.log(reason)
-	})
 	const room = joinOrCreate() || getRoomName()
+	
 	if(joinOrCreate()) {
 		socket.join(room, () => {
 			roomList[room].push(socketID)
 			if(roomList[room].length === 2) {
 				io.to(room).emit('startGame')
+				console.log(roomList)
 			}
 		})
 	} else {
@@ -28,6 +27,14 @@ io.on('connection', socket => {
 			console.log(roomList)
 		})
 	}
+
+	socket.on('disconnect', (reason) => {
+		socket.leave(room)
+		const index = roomList[room].indexOf(socketID)
+		roomList[room].splice(index, 1)
+		io.to(room).emit('leaveGame')
+		console.log(roomList)
+	})
 })
 
 
